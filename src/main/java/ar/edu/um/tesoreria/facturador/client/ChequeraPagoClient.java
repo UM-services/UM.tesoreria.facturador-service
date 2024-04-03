bcff1bd4517b5bf1eb9177aa7502d86a50d3450b
dqmdz <daniel.quinterospinto@gmail.com>
2024-04-03 20:20:15 -0300

    Chequera Pago DTO Added

--- src/main/java/ar/edu/um/tesoreria/facturador/client/ChequeraPagoClient.java
+++ src/main/java/ar/edu/um/tesoreria/facturador/client/ChequeraPagoClient.java
@@ -1,25 +1,18 @@
 package ar.edu.um.tesoreria.facturador.client;
 
 import ar.edu.um.tesoreria.facturador.kotlin.model.dto.ChequeraPagoDto;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
-@Component
-public class ChequeraPagoClient {
+@FeignClient(name = "core-service/chequerapago")
+public interface ChequeraPagoClient {
 
-    public List<ChequeraPagoDto> pendientesFactura(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime fechaPago) {
-        String baseUrl = "http://192.168.201.211:8092/chequerapago";
-        WebClient webClient = WebClient.builder().baseUrl(baseUrl).build();
-        Flux<ChequeraPagoDto> response = webClient.get().uri("/pendientesFactura/" + fechaPago).retrieve()
-                .bodyToFlux(ChequeraPagoDto.class);
-        return response.collectList().block();
-    }
+    @GetMapping("/pendientesFactura/{fechaPago}")
+    List<ChequeraPagoDto> pendientesFactura(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime fechaPago);
 
 }
---
+++ src/main/java/ar/edu/um/tesoreria/facturador/kotlin/model/dto/TipoPagoDto.kt
@@ -0,0 +1,8 @@
+package ar.edu.um.tesoreria.facturador.kotlin.model.dto
+
+data class TipoPagoDto(
+
+        var tipoPagoId: Int? = null,
+        var nombre: String = ""
+
+)
