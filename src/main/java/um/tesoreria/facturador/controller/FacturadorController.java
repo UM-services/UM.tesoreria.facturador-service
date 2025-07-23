a04a412b8100aea122bd6b7f2c8c5beb3cfc1162
dqmdz <daniel.quinterospinto@gmail.com>
2025-07-23 14:58:25 -0300

    Consul migration

--- src/main/java/um/tesoreria/facturador/controller/FacturadorController.java
+++ src/main/java/um/tesoreria/facturador/controller/FacturadorController.java
@@ -1,6 +1,5 @@
 package um.tesoreria.facturador.controller;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +20,6 @@ public class FacturadorController {
         return new ResponseEntity<>(service.facturaPendientes(), HttpStatus.OK);
     }
 
-    @Scheduled(cron = "0 0 2 * * *")
-    public ResponseEntity<Void> facturaPendientesScheduled() {
-        service.facturaPendientes();
-        return ResponseEntity.ok().build();
-    }
-
     @GetMapping("/facturaOne/{chequeraPagoId}")
     public ResponseEntity<String> facturaOne(@PathVariable Long chequeraPagoId) {
         return new ResponseEntity<>(service.facturaOne(chequeraPagoId), HttpStatus.OK);
@@ -40,12 +33,6 @@ public class FacturadorController {
     @GetMapping("/sendOne/recibo/{facturacionElectronicaId}")
     public ResponseEntity<String> sendOneByFacturacionElectronicaId(@PathVariable Long facturacionElectronicaId) {
         return new ResponseEntity<>(service.sendOneByFacturacionElectronicaId(facturacionElectronicaId), HttpStatus.OK);
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    public ResponseEntity<Void> sendFacturasPendientesScheduled() {
-        service.sendFacturasPendientes();
-        return ResponseEntity.ok().build();
     }
 
 }
