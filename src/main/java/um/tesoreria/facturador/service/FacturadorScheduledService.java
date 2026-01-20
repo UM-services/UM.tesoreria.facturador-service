562a4e1f6528fd6bfb64db03ea42ed752ebe5b2c
dqmdz <daniel.quinterospinto@gmail.com>
2026-01-20 10:32:06 -0300

    Corrigiendo la url de core

--- src/main/java/um/tesoreria/facturador/service/FacturadorScheduledService.java
+++ src/main/java/um/tesoreria/facturador/service/FacturadorScheduledService.java
@@ -1,18 +1,16 @@
 package um.tesoreria.facturador.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnProperty(name = "app.testing", havingValue = "false", matchIfMissing = true)
+@RequiredArgsConstructor
 public class FacturadorScheduledService {
 
     private final FacturadorService service;
-
-    public FacturadorScheduledService(FacturadorService service) {
-        this.service = service;
-    }
 
     @Scheduled(cron = "0 0 2 * * *")
     public void facturaPendientesScheduled() {
